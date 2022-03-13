e9e582fb6c8f1901902cddf11067f16acef63d56
Lucca Araujo <lucca.ara7@gmail.com>
2022-03-13 14:50:27 -0700

    add ability to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,55 @@
-var buttonEl = document.querySelector("#save-task"); // button
-var tasksToDoEl = document.querySelector("#tasks-to-do"); // ul
-
-
-
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
-
-  buttonEl.addEventListener("click", createTaskHandler);
-
 // steps:
 //Create a new task item.
 //Styte the new task item.
 //Add the text.
 //Append this element to the task list.
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var taskFormHandler = function(event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  //the value of the taskNameInput variable will be the text we entered in the <input> element.
+    //When we use square brackets [ ] in a selector, we're trying to select an HTML element by one of its attributes. 
+    //In this case, we're selecting the <input> 
+    //element on the page that has a name attribute set to a value of "task-name".
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+  }
+  formEl.reset();
+  
+  // package up data as an object
+  var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
+
+
+
+
+
